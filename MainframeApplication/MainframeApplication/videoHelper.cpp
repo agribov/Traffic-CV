1938f3f5daae5e63cabb4a4a970bbc0c33e21b8d
Alex Gribov <alexander_gribov@student.uml.edu>
2017-02-12 21:52:02 -0500

    Cleaned up and added frame number

--- MainframeApplication/MainframeApplication/videoHelper.cpp
+++ MainframeApplication/MainframeApplication/videoHelper.cpp
@@ -12,6 +12,38 @@
 
 #include "globals.h"
 #include "videoHelper.h"
+#include "vehicle.h"
+#include "vehicleTracker.h"
+
+void processVideo(VideoCapture* pCap) {
+
+	//read input data. ESC or 'q' for quitting
+	keyboard = 0;
+	VehicleTracker tracker;
+
+	Mat frame, outputFrame;
+
+	while (keyboard != 'q' && keyboard != 27) {
+
+		//read the current frame
+		if (!(*pCap).read(frame)) {
+			cerr << "Unable to read next frame." << endl;
+			cerr << "Exiting..." << endl;
+			exit(EXIT_FAILURE);
+		}
+
+		tracker.update(frame);
+		outputFrame = tracker.getTrackedFrame();
+		addFrameNumber(outputFrame, pCap);
+
+		imshow("Video Capture", outputFrame);
+		imshow("Debug 1", tracker.getDilated());
+		imshow("Debug 2", tracker.getEroded());
+
+		//get the input from the keyboard
+		keyboard = (char)waitKey(30);
+	}
+}
 
 VideoCapture* initializeVideo(char* videoFilename) {
 	//create the capture object
@@ -29,16 +61,80 @@ void releaseVideo(VideoCapture* pCap) {
 	(*pCap).release();
 }
 
-void dilateFrame(Mat inputFrame, Mat* p_returnFrame) {
-	//Morph type set to Rectangle, 3x3 rectangle - arbitrary size.
-	Mat dElement = getStructuringElement(MORPH_RECT, Size(3, 3));
-	//perform dilation
-	dilate(inputFrame, *p_returnFrame, dElement);
-	return;
+void addFrameNumber(Mat &frame, VideoCapture *pCap) {
+	//get the frame number and write it on the current frame
+	stringstream ss;
+	rectangle(frame, cv::Point(10, 2), cv::Point(100, 20),
+		cv::Scalar(255, 255, 255), -1);
+	ss << (*pCap).get(CAP_PROP_POS_FRAMES);
+	string frameNumberString = ss.str();
+	putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
+		FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
 }
-void erodeFrame(Mat outputFrame, Mat* p_returnFrame) {
-	//Morph type set to Rectangle, 3x3 rectangle - arbitrary size.
-	Mat eElement = getStructuringElement(MORPH_RECT, Size(3, 3));
-	erode(outputFrame, *p_returnFrame, eElement);
-	return;
+
+// NOT USED, here as a historic monument
+/*
+void OLDprocessVideo(VideoCapture* pCap) {
+
+	//read input data. ESC or 'q' for quitting
+	keyboard = 0;
+
+	Mat frame, threshFrame, adaptiveFrame; //current frame
+	Mat fgMaskKNN;
+
+	while (keyboard != 'q' && keyboard != 27) {
+
+		//read the current frame
+		if (!(*pCap).read(frame)) {
+			cerr << "Unable to read next frame." << endl;
+			cerr << "Exiting..." << endl;
+			exit(EXIT_FAILURE);
+		}
+
+		colorThreshold(frame, &threshFrame);
+		//Test adaptive thresholding
+		//cvtColor(frame, adaptiveFrame, COLOR_BGR2GRAY);	//BGR to gray color conversion
+		//adaptiveThreshold(adaptiveFrame, adaptiveFrame, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 3, 0);
+
+		for (int i = 0; i < erosionVal; i++)
+		{
+			erodeFrame(threshFrame, &threshFrame);
+			//erodeFrame(adaptiveFrame, &adaptiveFrame);
+		}
+
+		for (int i = 0; i < dilationVal; i++)
+		{
+			dilateFrame(threshFrame, &threshFrame);
+			//dilateFrame(adaptiveFrame, &adaptiveFrame);
+		}
+
+		//update the background model
+		pMOG2->apply(threshFrame, fgMaskMOG2);
+		pKNN->apply(threshFrame, fgMaskKNN);
+
+		//get the frame number and write it on the current frame
+		stringstream ss;
+		rectangle(frame, cv::Point(10, 2), cv::Point(100, 20),
+			cv::Scalar(255, 255, 255), -1);
+		ss << (*pCap).get(CAP_PROP_POS_FRAMES);
+		string frameNumberString = ss.str();
+		putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
+			FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+
+		//Run Thresholding
+		//thresholding(frame, &threshFrame);
+
+		//show current frame and masks
+		imshow("Video Capture", frame);
+		imshow("FG Mask KNN", fgMaskKNN);
+		//imshow("FG Mask MOG 2", fgMaskMOG2);
+		imshow("Thresholded", (threshFrame));
+		//imshow("Adaptive Thresholding", adaptiveFrame);
+
+		//get the input from the keyboard
+		keyboard = (char)waitKey(30);
+	}
+	//delete capture object
+	releaseVideo(pCap);
 }
+*/
